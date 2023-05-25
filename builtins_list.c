78a829b57be1a1dcc8bf95b54349b66664efc57b
Allan Njoroge <62743302+Heraw254@users.noreply.github.com>
2023-05-25 15:19:04 +0300

    Update builtins_list.c

--- builtins_list.c
+++ builtins_list.c
@@ -5,6 +5,7 @@
  * @data: struct for the program's data
  * Return: Returns the return of the function executed is there is a match,
  * otherwise returns -1.
+ *
  **/
 int builtins_list(data_of_program *data)
 {
